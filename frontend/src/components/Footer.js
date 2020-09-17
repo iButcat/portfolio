84955f7c3caa9bf6ce6b96e297cae3e944e6178a
iButcat <Butcat@protonmail.com>
2020-09-17 12:27:08 +0200

    change for functionnal components

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="contact-content text-center">
-                <p>Copyright &copy; 2020  All Rights Reserved.</p>
-              </div>
-            </div>
+const Footer = () => (
+  <footer>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-6 offset-lg-3">
+          <div className="contact-content text-center">
+            <p>Copyright &copy; 2020  All Rights Reserved.</p>
           </div>
         </div>
-      </footer>
-   )
-  }
-}
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer;
--- frontend/src/components/Jumbotron.js
+++ frontend/src/components/Jumbotron.js
@@ -4,27 +4,22 @@ import PropTypes from 'prop-types';
 import Typed from 'react-typed';
 
 
-export default class Jumbotron extends Component {
-  constructor(props) {
-    super (props);
-  }
-  render() {
-    return (
-      <div className="jumbotron" id="home">
-        <div className="container">
-          <h1 className="display-4">Hello World, I'm Alexis</h1>
-          <div>
-            <p className="lead">
-              <Typed strings={[
-              "Front End Developer",
-              "Back End Developer",
-              "Junior Web Developer"
-            ]}
-            typeSpeed={80} backDelay={1100} backSpeed={30} loop />
-          </p>
-        </div>
-      </div>
+const Jumbotron = () => (
+  <div className="jumbotron" id="home">
+    <div className="container">
+      <h1 className="display-4">Hello World, I'm Alexis</h1>
+      <div>
+        <p className="lead">
+          <Typed strings={[
+          "Front End Developer",
+          "Back End Developer",
+          "Junior Web Developer"
+        ]}
+        typeSpeed={80} backDelay={1100} backSpeed={30} loop />
+      </p>
     </div>
-  )
-}
-}
+  </div>
+</div>
+)
+
+export default Jumbotron;
