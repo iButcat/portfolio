9a217d9e07e6a62fb5de87c56d7fed607f7b008b
iButcat <Butcat@protonmail.com>
2020-09-20 12:09:33 +0200

    test project api

--- backend/pyportfolio/tests/tests_project_api.py
+++ backend/pyportfolio/tests/tests_project_api.py
@@ -1,14 +1,25 @@
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIRequestFactory
 from pyportfolio.models import Project
-from django.test import Client
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class ProjectAPITest(APITestCase):
-    client = Client()
 
     def setUp(self):
+        self.factory = APIRequestFactory()
+        self.response = Response()
         self.project = Project.objects.create(
-        title = "Test title",
-        description = "My test description",
-        technology = "Django",
+        title = "Title test",
+        description = "Description test",
+        technology = "Technology test"
         )
+
+    def test_project_post(self):
+        post_project_to_api = self.factory.post('project/',
+        {'data': self.project})
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+
+    def test_project_get(self):
+        request = self.factory.get('api/project/')
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
