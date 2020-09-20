0820b66fb94b7d03b3dd098bd6f29e66ad0cf1d4
iButcat <Butcat@protonmail.com>
2020-09-20 14:20:50 +0200

    test contact api

--- backend/pyportfolio/tests/tests_contact_api.py
+++ backend/pyportfolio/tests/tests_contact_api.py
@@ -5,31 +5,23 @@ from rest_framework.response import Response
 from pyportfolio.models import Contact
 from pyportfolio.views import ContactCreateView
 
-response = Response()
 
 class CreateNewExperimentTest(APITestCase):
 
     def setUp(self):
-        self.name = 'Test'
-        self.email = 'email@test.com'
-        self.message = 'Some text'
-        contact = Contact.objects.create(
-        name=self.name,
-        email=self.email,
-        message=self.message
+        self.contact = Contact.objects.create(
+        name="Test",
+        email="email@test.com",
+        message="Message test"
         )
+        self.factory = APIRequestFactory()
+        self.response = Response()
 
     def test_valid_contact_post(self):
-        factory = APIRequestFactory()
-        request = factory.post('contact/', {
+        request = self.factory.post('contact/', {
         'name': 'test',
         'email': 'email@test.com',
         'message': 'some message'
         },
         format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_invalid_contact_post(self):
-        factory = APIRequestFactory()
-        request = factory.post('contact/',{})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
