3dd8976e401aac7c4e94b1de063b26cba2f2efd2
iButcat <Butcat@protonmail.com>
2020-09-22 11:39:47 +0200

    card projects

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,10 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
-import Button from '@material-ui/core/Button';
 import { AiFillGithub } from "react-icons/ai";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-import Slider from './Slider';
+const Style = {
+  backgroundColor: 'inherit',
+  color: 'inherit'
+}
 
 const BASE_URL = "https://alexismorin.herokuapp.com/portfolio/api/project/";
 //const BASE_URL = "http://127.0.0.1:8000/portfolio/api/project";
@@ -27,29 +37,42 @@ export default class Project extends Component {
   render() {
     return (
         <div className="project">
-          <Slider>
+          <div className="container-fluid">
+            <div className="row">
           {this.state.projects.map((project, id) => (
-            <div
-              key={project.id}
-              className="col-sm-4 col-md-6">
-              <div>
-              <h1 className="text-center">{project.title}</h1>
-              <p className="text-left">{project.description}</p>
-              <p className="text-center"><Link to={`/${project.id}`} className="stretched-link">
-                Continue reading
-              </Link></p>
-              <Button size="small" color="primary">
-                <a href={project.Github} target="_blank">
-                  <AiFillGithub size={40} style={{ color: "black" }}/>
-                </a>
-              </Button>
-              </div>
+            <div key={project.id} className="col-sm-12 col-md-4 col-xs-12">
+            <Card className="card" style={Style}>
+              <CardActionArea>
+                <CardMedia>
+                  <img src={project.image} alt="project-img" id="project-img"/>
+                </CardMedia>
+                <CardContent>
+                  <Typography gutterBottim variant="h5" component="h2">
+                    {project.title}
+                  </Typography>
+                  <Typography variant="body2" color="white" component="p">
+                    {project.description.substring(0, 250)}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">
+                  <Link to={`${project.id}`}>Learn More</Link>
+                </Button>
+                <Button size="small" color="primary">
+                  <a href={project.Github} target="_blank">
+                    Source Code
+                    <AiFillGithub size={20} style={{ color: "black" }}/>
+                  </a> 
+                </Button>
+              </CardActions>
+            </Card>
           </div>
-
           )
         )}
-         </Slider>
       </div>
+    </div>
+  </div>
   )
 }
 }
