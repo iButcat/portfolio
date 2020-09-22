cd8f5842d94735b9128c22fc60ca6798236d3757
iButcat <Butcat@protonmail.com>
2020-09-22 11:39:34 +0200

    card education

--- frontend/src/components/Education.js
+++ frontend/src/components/Education.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+import { AiFillGithub } from "react-icons/ai";
+import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,6 +10,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
 
 const Style = {
   backgroundColor: 'inherit',
@@ -17,14 +21,14 @@ const BASE_URL = "https://alexismorin.herokuapp.com/portfolio/api/education/"
 
 export default class Education extends Component {
   state = {
-    projects: []
+    educations: []
   }
 
   componentDidMount() {
     fetch(BASE_URL)
       .then(res => res.json())
-      .then(projects => {
-        this.setState({ projects })
+      .then(educations => {
+        this.setState({ educations })
       });
   }
 
@@ -34,28 +38,29 @@ export default class Education extends Component {
         <div className="education">
           <h3 className="text-center" id="portfolio-title">
             Certifications And Books</h3>
+
           <div className="row">
-            {this.state.projects.map((project, id) => (
-              <div className="col-sm-12 col-md-4 col-xs-12" key={project.id}>
+            {this.state.educations.map((education, id) => (
+              <div className="col-sm-12 col-md-4 col-xs-12" key={education.id}>
                 <Card className="card" style={Style}>
                   <CardActionArea>
                     <CardMedia>
-                      <img src={project.image}/>
+                      <img src={education.image} alt="project-img" id="project-img"/>
                     </CardMedia>
                     <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2"
-                        style={Style}>
-                        {project.title}
+                      <Typography gutterBottim variant="h5" component="h2">
+                        {education.title}
                       </Typography>
-                      <Typography
-                        variant="body2"
-                        color="textSecondary"
-                        component="p"
-                        style={Style}>
-                        {project.description.substring(0, 250)}...
+                      <Typography variant="body2" color="white" component="p">
+                        {education.description.substring(0, 250)}
                       </Typography>
                     </CardContent>
                   </CardActionArea>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      <Link to={`${education.id}`}>Learn More</Link>
+                    </Button>
+                  </CardActions>
                 </Card>
               </div>
             )
