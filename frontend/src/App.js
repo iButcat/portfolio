0d7302a5f620173ceb373fca60aa0bee89dec1ac
iButcat <Butcat@protonmail.com>
2020-09-23 17:57:31 +0200

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/:id" component={DetailEducation}/>
+        <Route exact path="/:id" component={DetailEducation} />
         <Route exact path="/:id" component={DetailProject} />
         <Fragment>
         <Layout>
